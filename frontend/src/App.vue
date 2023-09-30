5560946505e1eb81c3bf2a99003742d20c35afa1
Cem Gülümser <mgulumser@my.aci.k12.tr>
2023-10-01 00:43:22 +0300

    Sidebar.vue progress

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,12 @@
+<script setup>
+import Sidebar from "@/components/sidebar/Sidebar.vue"
+</script>
+
 
 <template>
-
+  <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+  <Sidebar></Sidebar>
 </template>
 
 <script>
-
 </script>
---
+++ frontend/src/components/sidebar/Sidebar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Sidebar_section from "@/components/sidebar/Sidebar_section.vue";
+</script>
+
+<template>
+  <div class="sidebar">
+    <Sidebar_section icon="fa fa-home" label="Home"/>
+    <Sidebar_section icon="fa fa-sort" label="Categories"/>
+    <Sidebar_section icon="fa fa-bookmark" label="Profile"/>
+    <Sidebar_section icon="fa fa-user" label="Bookmarks"/>
+  </div>
+</template>
+
+<script>
+import '@/components/sidebar/sidebar.css'
+
+export default {
+  name: "Sidebar",
+  components: ['Sidebar_section']
+}
+</script>
