c061efe59a7bf568b7059e61680b253f6c0ce4c9
cmglmsr <mgulumser@my.aci.k12.tr>
2023-10-06 00:55:39 +0300

    latest headlines prototype

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <Feed></Feed>
+  <Sidebar></Sidebar>
+  <Top_news_list></Top_news_list>
 </template>
 
 <script setup>
 import Sidebar from "@/components/sidebar/Sidebar.vue"
 import Feed from "./components/common/Card.vue"
+import Top_news_list from "@/components/newslist/Top_news_list.vue";
 </script>
 
 
