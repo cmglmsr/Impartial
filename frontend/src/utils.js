ab1a1dfca07ca6c8977e5d59918adc3a63ef8991
Cem Gülümser <82974344+cmglmsr@users.noreply.github.com>
2024-04-16 00:18:32 +0300

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let isDev = import.meta.env.DEV
-const serverUrl = isDev ? "https://final-lb-2105412396.us-east-1.elb.amazonaws.com" : "https://final-lb-2105412396.us-east-1.elb.amazonaws.com"
+const serverUrl = isDev ? "https://api.newsimpartial.com" : "https://api.newsimpartial.com"
 export const axiosInstance = axios.create({
     baseURL : serverUrl + "/home",
     headers: {
