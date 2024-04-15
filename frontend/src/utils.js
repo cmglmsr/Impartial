0a4d74d7b3b90c1abf6f7907d0848f76da298b11
nisayilmaz <nisayilmaz221@gmail.com>
2024-04-15 23:31:13 +0300

    ..

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let isDev = import.meta.env.DEV
-const serverUrl = isDev ? "http://54.166.185.242:8080" : "http://54.166.185.242:8080"
+const serverUrl = isDev ? "http://final-lb-2105412396.us-east-1.elb.amazonaws.com:8080" : "http://final-lb-2105412396.us-east-1.elb.amazonaws.com:8080"
 export const axiosInstance = axios.create({
     baseURL : serverUrl + "/home",
     headers: {
