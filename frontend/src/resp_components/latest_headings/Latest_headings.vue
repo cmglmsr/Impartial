5fc30ba4cbd5796da5010825a0a606f921be674a
aslikaraman <asli.karaman92@gmail.com>
2024-05-01 18:18:44 +0300

    latest head. read more added and class. reas. popup scroll added

--- frontend/src/resp_components/latest_headings/Latest_headings.vue
+++ frontend/src/resp_components/latest_headings/Latest_headings.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
     <div class="Latest-heading-text">Latest Headings</div>
-    <div v-for="news in latestNews" class="mb-5 col-12 card" style="width: 60%; height: 20%; margin-top: 1vw">
-      <div class="name-list-element" style="font-size: 1vw">{{ news.title }}</div>
+    <div
+      v-for="news in latestNews"
+      class="mb-5 col-12 card"
+      style="width: 60%; height: 20%; margin-top: 1vw"
+    >
+      <div class="name-list-element" style="font-size: 0.8vw">
+        {{ news.title }}
+      </div>
       <div style="display: flex; justify-content: space-between">
         <div
           class="name-list-element"
-          style="text-align: left; font-size: 0.7vw"
+          style="text-align: left; font-size: 0.5vw"
         >
-          Date: {{formatDate(news.publishDate) }}
+          Date: {{ formatDate(news.publishDate) }}
         </div>
         <div
           class="name-list-element"
-          style="text-align: right; font-size: 0.7vw"
+          style="text-align: right; font-size: 0.5vw"
         >
           Source: {{ news.source }}
         </div>
       </div>
+      <button
+        style="
+          text-decoration: none;
+          padding: 0.5vw 0.3vw 0.5vw 0.3vw;
+          border: none;
+          border-radius: 2vw;
+          font-weight: 600;
+          font-size: 0.9vw;
+          background-color: #e0efff;
+          color: #11101d;
+          margin-top: 1vw;
+        "
+      @click="redirectToReadMore">
+        Read More
+      </button>
     </div>
   </div>
 </template>
@@ -25,17 +46,20 @@
 import moment from "moment";
 
 export default {
-  props:{
-    latestNews : Array,
+  props: {
+    latestNews: Array,
     header: String,
     date: String,
-    source: String
+    source: String,
   },
   methods: {
-      formatDate(dateInput, format = "DD.MM.YYYY") {
-          return moment(dateInput).format(format);
-      },
-  }
+    formatDate(dateInput, format = "DD.MM.YYYY") {
+      return moment(dateInput).format(format);
+    },
+    redirectToReadMore() {
+      this.$router.push({ name: "read-more-page", params: { id: 0 } });
+    },
+  },
 };
 </script>
 
--- frontend/src/resp_components/popups/show_classification_reasoning_popup.vue
+++ frontend/src/resp_components/popups/show_classification_reasoning_popup.vue
@@ -1,70 +1,75 @@
 <template>
-    <div class="add-comment-popup" v-if="classificationReasoningPopup">
-        <div class="add-comment-overlay"></div>
-        <div class="add-comment-popup-content">
-            <div v-if="!this.loading" class="add-comment-controls-versions">
-                <p class="reasoning">{{ reasoning }}</p>
-            </div>
-            <div v-if="this.loading" class="d-flex flex-column  align-items-center">
-
-                <div class="spinner-border" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-                <div class="my-3">
-                    <span class="text-muted">Generating reasoning...</span>
-                </div>
-
-            </div>
-            <div
-                    class="add-comment-controls-versions"
-                    style="display: flex; justify-content: center"
-            >
-                <button v-if="!loading" class="add-comment-submit-btn" @click="closePopup">
-                    Close
-                </button>
-            </div>
+  <div class="add-comment-popup" v-if="classificationReasoningPopup">
+    <div class="add-comment-overlay"></div>
+    <div class="add-comment-popup-content">
+      <div
+        v-if="!this.loading"
+        class="add-comment-controls-versions"
+        style="max-height: 80vh; overflow-y: auto"
+      >
+        <p class="reasoning" style="font-size: 0.8vw">{{ reasoning }}</p>
+      </div>
+      <div v-if="this.loading" class="d-flex flex-column align-items-center">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
+        <div class="my-3">
+          <span class="text-muted">Generating reasoning...</span>
+        </div>
+      </div>
+      <div
+        class="add-comment-controls-versions"
+        style="display: flex; justify-content: center"
+      >
+        <button
+          v-if="!loading"
+          class="add-comment-submit-btn"
+          @click="closePopup"
+        >
+          Close
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import {axiosInstance} from "@/utils";
+import { axiosInstance } from "@/utils";
 
 export default {
   props: {
-    article : Object,
+    article: Object,
     classificationReasoningPopup: Boolean,
   },
   data() {
     return {
-        reasoning: "",
-        loading: false
-    }
+      reasoning: "",
+      loading: false,
+    };
   },
-    methods: {
-        closePopup() {
-            this.reasoning = ""
-            this.$emit("close-popup");
-        },
+  methods: {
+    closePopup() {
+      this.reasoning = "";
+      this.$emit("close-popup");
     },
-    watch: {
-        async classificationReasoningPopup(newVal) {
-            if (newVal && !this.loading) {
-                try {
-                  this.loading = true
-                  const response = await axiosInstance.post(`/news/reasoning`, {
-                      "articleBody": this.article.content.replace(/[^\w\s.,!?]|[\r\n]/g, "").replace(/\n/g, " "),
-                      "currentAlignment": this.article.alignment,
-                  });
-                  this.reasoning = response.data
-                }
-                catch (e) {
-
-                }
-                this.loading = false
-            }
-        }
-    }
+  },
+  watch: {
+    async classificationReasoningPopup(newVal) {
+      if (newVal && !this.loading) {
+        try {
+          this.loading = true;
+          const response = await axiosInstance.post(`/news/reasoning`, {
+            articleBody: this.article.content
+              .replace(/[^\w\s.,!?]|[\r\n]/g, "")
+              .replace(/\n/g, " "),
+            currentAlignment: this.article.alignment,
+          });
+          this.reasoning = response.data;
+        } catch (e) {}
+        this.loading = false;
+      }
+    },
+  },
 };
 </script>
 
@@ -93,7 +98,8 @@ export default {
 }
 
 .add-comment-popup-content {
-  top: 15%;
+  top: 3%;
+  bottom: 3%;
   position: relative;
   width: 90%;
   max-width: 500px;
@@ -146,81 +152,81 @@ export default {
 }
 
 @media screen and (max-width: 768px) {
-    .reasoning{
-        font-size: 2vw;
-    }
+  .reasoning {
+    font-size: 2vw;
+  }
   .add-comment-popup {
-  box-sizing: border-box;
-  width: 20%;
-  height: 10%;
-  z-index: 9999;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 10%; /* Adjust this value to position the popup higher */
-}
-.add-comment-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-}
+    box-sizing: border-box;
+    width: 20%;
+    height: 10%;
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 10%; /* Adjust this value to position the popup higher */
+  }
+  .add-comment-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
 
-.add-comment-popup-content {
+  .add-comment-popup-content {
     top: -20%;
     left: 4%;
-  position: relative;
-  width: 50%;
-  background: #fff;
-  padding: 4vw;
-  border-radius: 20px;
-  box-shadow: 0px 2px 2px 5px rgba(0, 0, 0, 0.05);
-}
+    position: relative;
+    width: 50%;
+    background: #fff;
+    padding: 4vw;
+    border-radius: 20px;
+    box-shadow: 0px 2px 2px 5px rgba(0, 0, 0, 0.05);
+  }
 
-.h2-title {
-  font-size: 4vw;
-}
+  .h2-title {
+    font-size: 4vw;
+  }
 
-.comment-input {
-  width: 100%;
-  margin-left: 1vw;
-  height: 100px;
-  resize: vertical;
-  margin-bottom: 15px;
-}
+  .comment-input {
+    width: 100%;
+    margin-left: 1vw;
+    height: 100px;
+    resize: vertical;
+    margin-bottom: 15px;
+  }
 
-.add-comment-controls-versions {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  .add-comment-controls-versions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-.add-comment-submit-btn,
-.add-comment-close-btn {
-  background: transparent;
-  color: #11101d;
-  border: 0.5vw solid #11101d;
-  padding: 1vw 2vw;
-  cursor: pointer;
-  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
-  border-radius: 2vw;
-}
+  .add-comment-submit-btn,
+  .add-comment-close-btn {
+    background: transparent;
+    color: #11101d;
+    border: 0.5vw solid #11101d;
+    padding: 1vw 2vw;
+    cursor: pointer;
+    transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+    border-radius: 2vw;
+  }
 
-.add-comment-submit-btn:hover,
-.add-comment-close-btn:hover {
-  background-color: #11101d;
-  color: #fff;
-}
+  .add-comment-submit-btn:hover,
+  .add-comment-close-btn:hover {
+    background-color: #11101d;
+    color: #fff;
+  }
 
-.add-comment-popup
-  .add-comment-popup-content
-  .add-comment-controls-versions
-  .add-comment-submit-btn {
-  background: #11101d; 
-  color: #fff;
-  font-size: 2vw;
-}
+  .add-comment-popup
+    .add-comment-popup-content
+    .add-comment-controls-versions
+    .add-comment-submit-btn {
+    background: #11101d;
+    color: #fff;
+    font-size: 2vw;
+  }
 }
 </style>
